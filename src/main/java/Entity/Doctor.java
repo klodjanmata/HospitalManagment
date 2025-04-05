ebd5668e684699a94f8b82c9aab977ed6d47e949
megiahmetaj <megiahmetaj2005@gmail.com>
2025-04-05 11:57:10 +0200

    Doctor hibernate update

--- src/main/java/Entity/Doctor.java
+++ src/main/java/Entity/Doctor.java
@@ -1,73 +1,33 @@
 package Entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "Doctor")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "surname")
     private String surname;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Specialization")
     private String speciality;
+    @Column(name = "phoneNumber")
     private String phone;
+    @Column(name = "email")
     private String email;
-
-    public Doctor(){}
-
-    public Doctor(int id, String name, String surname, String speciality, String phone, String email) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.speciality = speciality;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setSpeciality(String speciality) {
-        this.speciality = speciality;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public String toString() {
