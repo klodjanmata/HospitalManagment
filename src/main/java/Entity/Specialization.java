2870bbc06f925bf7da04a94fe6c67497213d33b6
megiahmetaj <megiahmetaj2005@gmail.com>
2025-04-06 09:56:36 +0200

    changes

--- src/main/java/Entity/Specialization.java
+++ src/main/java/Entity/Specialization.java
@@ -7,4 +7,14 @@ public enum Specialization {
     EmergencyMedicine,
     FamilyMedicine,
     Pediatrics;
+
+    public static void printChoseSpecialization(){
+        System.out.println(
+                "1 - Allergy And Immunology\n" +
+                        "2 - Cardiology\n" +
+                        "3 - Dermatology\n" +
+                        "4 - EmergencyMedicine\n" +
+                        "5 - FamilyMedicine\n" +
+                        "6 - Pediatrics");
+    }
 }
